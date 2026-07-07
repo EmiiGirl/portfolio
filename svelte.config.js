ee971203adb12941798d36f228c4879a33a28a2c
Siva <sivasankar.sivakripa123@gmail.com>
2026-07-07 11:24:20 +0530

    image fix

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
+		}
+	}
+};
